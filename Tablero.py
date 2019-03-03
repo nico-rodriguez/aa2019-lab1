f7d0e9e384ebcad1c748dd7ab840c26df87a66d8
nico-rodriguez <marconicolasrodriguez@gmail.com>
2019-03-03 16:26:46 -0300

    Se mueven constantes globales a Constantes.py

--- Tablero.py
+++ Tablero.py
@@ -1,49 +1,4 @@
-# Constantes globales con las posiciones factibles del tablero
-# Se separan en tres listas para saber más fácilmente si una ficha
-# llegó a la punta opuesta o todavía no salio de la punta propia
-punta_negra = []
-punta_blanca = []
-parte_media = []
-
-# Para generar la forma de estrella, se estructura la inicialización en cinco loops
-# La casilla central de la estrella corresponde a la posición (0,0)
-# Se usan coordenadas cartesianas (i,j)
-# (algunos loops se hacen de arriba hacia abajo y otros al revés)
-
-# Punta superior de la estrella
-for i in range(5, 9, 1):
-	for j in range(-8+i, 9-i, 2):
-		punta_negra.append((i,j))
-
-# Parte central superior
-for i in range(1, 5, 1):
-	for j in range(-(8+i), 9+i, 2):
-		parte_media.append((i,j))
-
-# Fila horizontal central
-for j in range(-8, 9, 2):
-	parte_media.append((0,j))
-
-# Parte central inferior
-for i in range(-4, 0, 1):
-	for j in range(-8+i, 8+(-i)+1, 2):
-		parte_media.append((i,j))
-
-# Punta inferior de la estrella
-for i in range(-5, -9, -1):
-	for j in range(-(8+i), 9+i, 2):
-		punta_blanca.append((i,j))
-
-# Constante global con las direcciones posibles (hay que verificar si la dirección cae dentro del tablero)
-direcciones = [
-			(1,-1),		# noroeste
-			(1,1),		# noreste
-			(0,-2),		# oeste
-			(0,2),		# este
-			(-1,-1),	# suroeste
-			(-1,1)		# sureste
-		]
-
+from Constantes import *
 
 '''
 La clase tablero contiene las posiciones del tablero, las posiciones de las
@@ -177,9 +132,10 @@ class Tablero:
 			print()
 
 # tablero_actual es una instancia de Tablero
-# movimiento es una lista con dos tuplas: la primera es la posición vieja de una de las fichas;
-# la segunda tupla es la nueva posición de la ficha
-def nuevo_tablero(tablero_actual, movimiento):
+# ficha es una tupla con la posición de la ficha a mover
+# movimiento es una tupla con la nueva posición de la ficha
+def nuevo_tablero(tablero_actual, ficha, movimiento):
+
 	return Tablero()
 
 # Para debuggeing
