85acf5db99e2d060a39566a6e6fb48599a467472
Marco Nicolas Rodriguez Alvariza <marco.nicolas.rodriguez@fing.edu.uy>
2019-03-05 11:31:27 -0300

    Se agrega el argumento self a los métodos de la clase Juego

--- Juego.py
+++ Juego.py
@@ -9,23 +9,23 @@ class Juego:
                            "negras"  : jugador2 }
 
     #Aplica la jugada ingresada, modificando el ablero actual
-    def jugada(ficha, movimiento, color):
+    def jugada(self, ficha, movimiento, color):
         tipo_jugador = jugadores[color.value]
         jugada = mejor_jugada_para_jugador(tipo_jugador)
         (ficha,movimiento) = jugada
         self.tablero = self.tablero.actualizar_tablero(ficha, movimiento, color)
 
     #Retorna la tupla para el tablero actual
-    def obtener_tupla_tablero():
+    def obtener_tupla_tablero(self):
         return self.tablero.tupla
 
     #Retorna true si hay un ganador para el tablero actual
-    def hay_ganador():
+    def hay_ganador(self):
         return self.tablero.tupla.["fichas_blancas_en_punta_opuesta"] == 10 || 
         self.tablero.tupla.["fichas_negras_en_punta_opuesta"] == 10
     
     #Retorna el ganador solo en caso que haya (hay_ganador == true) sino retorna NULL
-    def ganador():
+    def ganador(self):
         if self.tablero.tupla.["fichas_blancas_en_punta_opuesta"] == 10:
             return Color.Blancas
         elif self.tablero.tupla.["fichas_negras_en_punta_opuesta"] == 10:
