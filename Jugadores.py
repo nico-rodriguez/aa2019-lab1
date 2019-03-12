e90e4bbc1121c755fc5ba920f09f6e52b6126c6e
d.susviela <daniel.susviela@fing.edu.uy>
2019-03-12 13:43:33 -0300

    agregados un par de todos

--- Jugadores.py
+++ Jugadores.py
@@ -57,7 +57,9 @@ class AI(Jugador):
         self.entrenando = entrenando
         self.factor_aprendizaje = factor_aprendizaje
         self.color_oponente = Color.Negras if color == Color.Blancas else Color.Blancas
-        
+
+    # FUNCIONES DEL ALGORITMO
+
     # Recibe la tupla que representa al tablero
     # Retorna la suma ponderada de los elementos de la tupla
     # El orden de los elementos de la tupla es el especificado en Tablero.py
@@ -116,40 +118,6 @@ class AI(Jugador):
         self.pesos[0] = self.pesos[0] + self.factor_aprendizaje * error_valoracion
         for i in range(len(tupla_tablero)):
             self.pesos[i+1] = self.pesos[i+1] + self.factor_aprendizaje * error_valoracion * tupla_tablero[i]
-    
-    def cargar_pesos(self, file_path):
-        try:
-            archivo_pesos=open(file_path,"r") 
-        except IOError: 
-            archivo_pesos.close()
-            return "Hubo un error al intentar abrir el archivo"
-        pesos = []
-        linea = archivo_pesos.readline()
-        while(linea != ""):
-            pesos.append(map(float, linea.split()))
-            linea = archivo_pesos.readline()
-        archivo_pesos.close()
-        return pesos
-
-    def guardar_pesos(self, archivo_pesos):
-        pesos_a_guardar = self.pesos.copy()
-        self.grabar_datos_en_disco(pesos_a_guardar, archivo_pesos)
-
-    def guardar_tupla(self, tupla_tablero, archivo_entrenamiento):
-        tupla_a_guardar = tupla_tablero.copy() + [self.valoracion(tupla_tablero)]
-        self.grabar_datos_en_disco(tupla_a_guardar, archivo_entrenamiento)
-    
-    def grabar_datos_en_disco(self, datos, ruta_archivo):
-        try:
-            archivo=open(ruta_archivo,"a+")
-        except IOError: 
-            archivo.close()
-            return "Hubo un error al intentar abrir/escribir el archivo"
-        linea_nueva = ""
-        for elemento in datos:
-            linea_nueva += (str(elemento) + " ")
-        archivo.write(linea_nueva + "\n")
-        archivo.close()
 
 	# Parsea el archvio con los valores de entrenamiento y realiza el ajuste de mínimos cuadrados
     def ajuste_minimos_cuadrados(self, archivo_entrenamiento):
@@ -168,6 +136,51 @@ class AI(Jugador):
             for i in range(len(tupla)-1):
                 self.pesos[i+1] = self.pesos[i+1] + self.factor_aprendizaje * error_valoracion * tupla[i]
         self.grabar_datos_en_disco(self.pesos, "pesos_finales.txt")
+
+    # FUNCIONES DE MANEJO DE ARCHIVOS
+
+    #  Lee los pesos del archivo de pesos y los carga en los atributos del jugador
+    def cargar_pesos(self, file_path):
+        try:
+            archivo_pesos=open(file_path,"r") 
+        except IOError: 
+            archivo_pesos.close()
+            return "Hubo un error al intentar abrir el archivo"
+        pesos = []
+        linea = archivo_pesos.readline()
+        while(linea != ""):
+            pesos.append(map(float, linea.split()))
+            linea = archivo_pesos.readline()
+        archivo_pesos.close()
+        return pesos
+
+    # Escribe en el archivo de pesos, los atributos de los pesos
+    def guardar_pesos(self, archivo_pesos):
+        pesos_a_guardar = self.pesos.copy()
+        self.grabar_datos_en_disco(pesos_a_guardar, archivo_pesos)
+
+    # Escribe en el archivo de entrenamiento correspondiente la tupla del tablero
+    # con su respectiva valoracion
+    # TODO: No se necesita esta funcion, la valuacion a grabar no deberia ser la de
+    # la tupla de entrada pero de la tupla que resulta del jugador que haga la sig
+    # movida
+    def guardar_tupla(self, tupla_tablero, archivo_entrenamiento):
+        tupla_a_guardar = tupla_tablero.copy() + [self.valoracion(tupla_tablero)]
+        self.grabar_datos_en_disco(tupla_a_guardar, archivo_entrenamiento)
+    
+    # dados unos datos en forma de lista y un archivo objetivo, 
+    # escribe esos elementos separados por un espacio
+    def grabar_datos_en_disco(self, datos, ruta_archivo):
+        try:
+            archivo=open(ruta_archivo,"a+")
+        except IOError: 
+            archivo.close()
+            return "Hubo un error al intentar abrir/escribir el archivo"
+        linea_nueva = ""
+        for elemento in datos:
+            linea_nueva += (str(elemento) + " ")
+        archivo.write(linea_nueva + "\n")
+        archivo.close()
 
 if __name__ == '__main__':
     jug = AI(Color.Negras, "AI1", None, True, 1)
