f90a7b67acddf934c238ef1f50fd79be09bc4465
Marco Nicolas Rodriguez Alvariza <marco.nicolas.rodriguez@fing.edu.uy>
2019-03-14 17:58:20 -0300

    Función para graficar los pesos individuales.

---
+++ Graficar.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+
+# Lee un archivo con la evolución de los valores de uno de los pesos.
+# Guarda una gráfica que muestra la evolución.
+# El formato esperado del archivo es un valor del peso por línea,
+# al final del archivo los últimos valores generados.
+# numero_peso es i para el peso wi.
+def graficar_peso(directorio_guardar, ruta_archivo_peso, numero_peso):
+	archivo_peso = open(ruta_archivo_peso, "r")
+	valores_peso = []
+
+	while True:
+		linea = archivo_peso.readline()
+		if linea == "":
+			break
+
+		linea = linea.split()[0]
+		print(linea)
+		linea = float(linea)
+		valores_peso.append(linea)
+
+	archivo_peso.close()
+	indice_peso = list(range(len(valores_peso)))
+
+	# Generar el gráfico
+	plt.plot(indice_peso, valores_peso)
+	plt.title("Evolucion del peso w_{num}".format(num=numero_peso))
+	plt.xlabel("Partidas")
+	plt.ylabel("Valor de w_{num}".format(num=numero_peso))
+	# Guardar los gráficos con nombre "peso_i.png"
+	plt.savefig(directorio_guardar + "/peso{i}.png".format(i=numero_peso))
